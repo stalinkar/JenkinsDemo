dd1a7b33b4189e56c01ca05a8408e8360641cbac
stalinkar <49714609+stalinkar@users.noreply.github.com>
2023-10-22 00:59:37 +0530

    Code Upload

---
+++ src/test/java/org/JenkinsDemoPack/VerifyPageTitle.java
@@ -0,0 +1,20 @@
+package org.JenkinsDemoPack;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class VerifyPageTitle {
+
+    @Test
+    public void startBrowser(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+        Assert.assertEquals(driver.getTitle(), "Google", "Title does not match");
+        driver.quit();
+    }
+}
