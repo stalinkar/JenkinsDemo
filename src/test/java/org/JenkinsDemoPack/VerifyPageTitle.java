ed8d7884c09119f2b96ca13be311095f8dd7d4b5
stalinkar <49714609+stalinkar@users.noreply.github.com>
2023-10-22 15:27:42 +0530

    browser parameter added
    parameter added

--- src/test/java/org/JenkinsDemoPack/VerifyPageTitle.java
+++ src/test/java/org/JenkinsDemoPack/VerifyPageTitle.java
@@ -3,15 +3,25 @@ package org.JenkinsDemoPack;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class VerifyPageTitle {
 
+    @Parameters("Browser")
     @Test
-    public void startBrowser(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+    public void startBrowser(String browserName){
+        System.out.println("Parameter is "+browserName);
+        WebDriver driver = null;
+        if (browserName.contains("Chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }else if (browserName.contains("Edge")){
+            WebDriverManager.chromedriver().setup();
+            driver = new EdgeDriver();
+        }
         driver.manage().window().maximize();
         driver.get("https://www.google.com/");
         Assert.assertEquals(driver.getTitle(), "Google", "Title does not match");
